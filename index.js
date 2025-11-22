09c10692d39007bd0ac50efc789688195dccc7ef
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-11-22 23:08:12 +0600

    user register

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
- 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 const uri = process.env.MONGO_URI;
- 
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +27,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //collection;
+    const zepShiftDB = client.db("ZepShit");
+    const userCollection = zepShiftDB.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const query = { email: newUser.email };
+      const existUser = await userCollection.findOne(query);
+      if (existUser) {
+        return res.send({ message: "user already Exist" });
+      }
+
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -40,7 +55,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
 
 app.listen(port, () => {
   console.log(`Zep-Shift listening on port ${port}`);
